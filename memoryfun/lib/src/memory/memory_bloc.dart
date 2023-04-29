3eea338da66489d487b7a7f77d1f245076a5f769
Helen Bohleber <helen-bohleber@web.de>
2023-04-29 18:15:55 +0200

    show and hide memory cards

---
+++ memoryfun/lib/src/memory/memory_bloc.dart
@@ -0,0 +1,138 @@
+import 'dart:math';
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:memoryfun/gen/assets.gen.dart';
+import 'package:memoryfun/src/memory/image_mapper.dart';
+import 'package:memoryfun/src/memory/memory_tile.dart';
+import 'package:memoryfun/src/memory/theme_set.dart';
+import 'package:riverbloc/riverbloc.dart';
+
+part 'memory_bloc.freezed.dart';
+
+@freezed
+class MemoryEvent with _$MemoryEvent {
+  const factory MemoryEvent.initGame(int gameSize, ThemeSet themeSet) =
+      _InitGame;
+  const factory MemoryEvent.handleTap(int tileIndex, int pairValue) =
+      _HandleTap;
+}
+
+@freezed
+class MemoryState with _$MemoryState {
+  const factory MemoryState.initial() = _Initial;
+  const factory MemoryState.loading() = _Loading;
+  const factory MemoryState.initialized(List<MemoryTile> memorySet) =
+      _Initialized;
+  const factory MemoryState.loadingResult() = _LoadingResult;
+  const factory MemoryState.matchResult(List<MemoryTile> memorySet) =
+      _MatchResult;
+  const factory MemoryState.winGame() = _WinGame;
+  const factory MemoryState.looseGame() = _looseGame;
+}
+
+class MemoryBloc extends Bloc<MemoryEvent, MemoryState> {
+  static final provider =
+      BlocProvider.autoDispose<MemoryBloc, MemoryState>((ref) {
+    ref.onDispose(() => ref.bloc.close());
+
+    return MemoryBloc(
+      imageMapper: ref.watch(ImageMapper.provider),
+    );
+  });
+
+  final ImageMapper imageMapper;
+  List<MemoryTile> memoryTiles = [];
+  ThemeSet? themeSet;
+  int? firstIndex;
+  int? firstPairValue;
+  List<int> hideTiles = [];
+
+  MemoryBloc({
+    required this.imageMapper,
+  }) : super(const MemoryState.initial()) {
+    on<_InitGame>(_initGame);
+    on<_HandleTap>(_handleTap);
+  }
+
+  Future _initGame(_InitGame event, Emitter<MemoryState> emit) async {
+    emit(const MemoryState.loading());
+
+    themeSet = event.themeSet;
+    var pairValues = [];
+    for (int i = 0; i < event.gameSize / 2; i++) {
+      pairValues.add(i);
+      pairValues.add(i);
+    }
+
+    for (int i = 0; i < event.gameSize; i++) {
+      var randomIndex = Random().nextInt(pairValues.length);
+      var value = pairValues[randomIndex];
+      pairValues.removeAt(randomIndex);
+
+      var tile = MemoryTile(
+        index: i,
+        pairValue: value,
+        image: Assets.food.background,
+        visible: false,
+      );
+      print(value);
+
+      memoryTiles.add(tile);
+    }
+
+    emit(MemoryState.initialized(memoryTiles));
+  }
+
+  Future _handleTap(
+    _HandleTap event,
+    Emitter<MemoryState> emit,
+  ) async {
+    emit(const MemoryState.loadingResult());
+    var index = memoryTiles.indexWhere((tile) => tile.index == event.tileIndex);
+    var oldIndex = memoryTiles.indexWhere((tile) => tile.index == firstIndex);
+
+    if (hideTiles.isNotEmpty) {
+      for (var hideTileIndex in hideTiles) {
+        _setTileVisibility(hideTileIndex, event.pairValue, false);
+      }
+
+      hideTiles = [];
+    }
+
+    _setTileVisibility(index, event.pairValue, true);
+
+    if (firstIndex != null) {
+      if (firstIndex == event.tileIndex) {
+        return;
+      } else {
+        if (firstPairValue == event.pairValue) {
+          memoryTiles[index].visible = true;
+          memoryTiles[oldIndex].visible = true;
+
+          firstIndex = null;
+          firstPairValue = null;
+          emit(MemoryState.matchResult(memoryTiles));
+          return;
+        } else {
+          memoryTiles[index].visible = false;
+          memoryTiles[oldIndex].visible = false;
+
+          hideTiles.add(index);
+          hideTiles.add(oldIndex);
+
+          firstIndex = null;
+          firstPairValue = null;
+          emit(MemoryState.matchResult(memoryTiles));
+        }
+      }
+    } else {
+      firstIndex = event.tileIndex;
+      firstPairValue = event.pairValue;
+    }
+    emit(MemoryState.matchResult(memoryTiles));
+  }
+
+  void _setTileVisibility(int index, int pairValue, bool visible) {
+    memoryTiles[index].image = imageMapper.map(themeSet!, pairValue, visible);
+  }
+}
--- memoryfun/lib/src/memory/memory_page.dart
+++ memoryfun/lib/src/memory/memory_page.dart
@@ -1,6 +1,12 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:memoryfun/src/memory/image_mapper.dart';
+import 'package:memoryfun/src/memory/memory_bloc.dart';
+import 'package:memoryfun/src/memory/theme_set.dart';
+import 'package:riverbloc/riverbloc.dart';
+
+import 'memory_tile.dart';
 
 @RoutePage()
 class MemoryPage extends ConsumerStatefulWidget {
@@ -12,13 +18,55 @@ class MemoryPage extends ConsumerStatefulWidget {
 
 class _MemoryPageState extends ConsumerState<MemoryPage> {
   @override
+  void initState() {
+    super.initState();
+
+    ref.read(MemoryBloc.provider.bloc).add(
+          const MemoryEvent.initGame(4, ThemeSet.food),
+        );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: const [
-          Text('This is the game page'),
-        ],
-      ),
+      body: ref.watch(MemoryBloc.provider).maybeWhen(
+            initialized: (memorySet) => _gridView(memorySet),
+            matchResult: (memorySet) => _gridView(memorySet),
+            orElse: () => const Text('loading'),
+          ),
     );
+  }
+
+  GridView _gridView(List<MemoryTile> memorySet) => GridView.count(
+        crossAxisCount: 4,
+        mainAxisSpacing: 8,
+        crossAxisSpacing: 8,
+        childAspectRatio: 1,
+        padding: const EdgeInsets.all(8),
+        children: _tiles(memorySet),
+      );
+
+  List<Widget> _tiles(List<MemoryTile> memorySet) {
+    List<Widget> tiles = [];
+    for (var tile in memorySet) {
+      final initTile = Material(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+        clipBehavior: Clip.antiAlias,
+        child: tile.image!.image(fit: BoxFit.cover, width: 200, height: 200),
+      );
+
+      print('visible: ' + tile.visible.toString());
+
+      tiles.add(InkWell(
+        onTap: () => tile.visible
+            ? {}
+            : ref
+                .read(MemoryBloc.provider.bloc)
+                .add(MemoryEvent.handleTap(tile.index, tile.pairValue)),
+        child: initTile,
+      ));
+    }
+
+    return tiles;
   }
 }
