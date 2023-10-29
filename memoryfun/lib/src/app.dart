37cdf4e1ab96bd9b6df90dd71b640de1cc5da0b4
Helen Bohleber <helen-bohleber@web.de>
2023-10-29 22:20:47 +0100

    disable landscape mode

--- memoryfun/lib/src/app.dart
+++ memoryfun/lib/src/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'utils/routing/app_router.dart';
 import 'utils/theme/app_color_mode.dart';
@@ -13,6 +14,11 @@ class MyApp extends ConsumerStatefulWidget {
 class _MyAppState extends ConsumerState<MyApp> {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
     ref
         .read(AppColorMode.provider)
         .setColorStyle(MediaQuery.highContrastOf(context));
